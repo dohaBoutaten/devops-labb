4937130be0d1b9f61641e3e0de839cfc6dd0c08e
dohaBoutaten <boutatendoha@gmail.com>
2026-04-23 15:33:52 +0200

    test1 auto trigger

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return  "Version 7 - Test GitHub CI/CD"
+    return  "Version 9 - Test GitHub CI/CD"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
